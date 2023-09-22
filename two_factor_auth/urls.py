ccafe799e75baec0509addbaa2acb3fe92abd648
Aleena K.V <139638350+aleenaallu@users.noreply.github.com>
2023-09-22 12:41:38 +0530

    Update urls.py

--- two_factor_auth/urls.py
+++ two_factor_auth/urls.py
@@ -6,12 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('2fa/',include('tfa_otp.urls')),
-    path('captcha/',include('captcha_app.urls')),
-    path('',include('encryption_app.urls')),
+
 
 ]
 
-# middleware = CaptchaMiddleware(
-#     customer_login_view = CustomerLogin.as_view(),
-#     superadmin_login_view=SuperAdminLogin.as_view()
-# )
